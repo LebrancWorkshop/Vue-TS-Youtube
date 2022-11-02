0eacb80508d3993864faed751da361725fcfed6e
lebrancconvas <lebrancconvas@gmail.com>
2022-11-03 03:07:01 +0700

    🎉 feat(init): init new project (Vite Vue3 TS)

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,14 +1,5 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-import App from "./App.vue";
-import router from "./router";
-
-import "./assets/main.css";
-
-const app = createApp(App);
-
-app.use(createPinia());
-app.use(router);
-
-app.mount("#app");
+createApp(App).mount('#app')
